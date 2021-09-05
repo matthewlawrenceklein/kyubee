f238de6443005ed9b319c32386483a57bfd30f03
Matthew Klein <mtt.l.kln@gmail.com>
2021-09-05 19:15:09 +0000

    basic card layout

--- src/DateCard.svelte
+++ src/DateCard.svelte
@@ -4,20 +4,26 @@
 
 
     onMount(() => {
-        console.log(date)
     });
 </script>
 
-<h3>{date.date}</h3>
-<ul>
-    <li>{date.title}</li>
-    <li>{date.details}</li>
-    {#each date.tags as tag}
-         <li>{tag}</li>
-    {/each}
-</ul>
+<div class='container card'>
+    <h3>{date.date}</h3>
+    <ul>
+        <li>{date.title}</li>
+        <li>{date.details}</li>
+        {#each date.tags as tag}
+             <li><strong>{tag}</strong></li>
+        {/each}
+    </ul>
+</div>
 
 <style>
+    .card {
+        width: 280px;
+        margin: 10px;
+        margin-top: 20px;
+    }
     /* your styles go here */
 </style>
 
--- src/constants.js
+++ src/constants.js
@@ -26,4 +26,52 @@ let events = [
         details : 'with the IRS',
         tags : ['important']
     },
+    {
+        date : '10/15/22',
+        title : 'doctor nicks',
+        details : 'bring ivermectin',
+        tags : ['doctor', 'errands', 'important']
+    },
+    {
+        date : '11/14/22',
+        title : 'eat a sandwich',
+        details : 'at the park with mary',
+        tags : ['date', 'social']
+    },
+    {
+        date : '02/14/22',
+        title : 'lollapalooza',
+        details : 'pack a big bag',
+        tags : ['music', 'social', 'travel']
+    },
+    {
+        date : '04/22/22',
+        title : 'pay my taxes',
+        details : 'with the IRS',
+        tags : ['important']
+    },
+    {
+        date : '10/15/22',
+        title : 'doctor nicks',
+        details : 'bring ivermectin',
+        tags : ['doctor', 'errands', 'important']
+    },
+    {
+        date : '11/14/22',
+        title : 'eat a sandwich',
+        details : 'at the park with mary',
+        tags : ['date', 'social']
+    },
+    {
+        date : '02/14/22',
+        title : 'lollapalooza',
+        details : 'pack a big bag',
+        tags : ['music', 'social', 'travel']
+    },
+    {
+        date : '04/22/22',
+        title : 'pay my taxes',
+        details : 'with the IRS',
+        tags : ['important']
+    },
 ]
