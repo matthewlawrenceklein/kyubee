c98acdecdc6b4254cd6635d359443e5d17346d35
Matthew Klein <mtt.l.kln@gmail.com>
2021-09-07 21:55:55 +0000

    avoid fetch loop

--- src/Dashboard.svelte
+++ src/Dashboard.svelte
@@ -1,39 +1,34 @@
 <script>
     import DateCard from './DateCard.svelte'
     import EventModal from './eventModal.svelte'
-    import { writable } from 'svelte/store'
-    import { onMount, beforeUpdate, afterUpdate } from 'svelte';
+    import { onMount } from 'svelte';
     export let calendarEvents
     export let eventModal
-    // $: calendarEvents = []
 
-    $: onMount(() =>{
+    onMount(() =>{
         getDates()
     })
 
-    $: beforeUpdate(() => {
-        getDates()
-    })
-
-    function getDates(){
+    let getDates = () => {
         fetch('http://localhost:3000/events')
             .then(resp => resp.json())
             .then(data => {
                 calendarEvents.set(data.events) // = data.events
+                console.log('fetched updated dates')
             })
     }
 </script>
 
 <div class='main'>
     {#if $eventModal}
-        <EventModal {eventModal}/>
+        <EventModal {eventModal} {getDates}/>
     {:else}
          <div class='container'>
              <!-- <button on:click={getDates}>get dates</button> -->
              <div class='row'>
                  {#if $calendarEvents}
                       {#each Object.values($calendarEvents) as dateObj}
-                          <DateCard {dateObj}/>
+                          <DateCard {dateObj} {getDates}/>
                       {/each}
                  {/if}
              </div>
