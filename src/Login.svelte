02a2af27cb08037831ac9da216f17923d2adb118
Matthew Klein <mtt.l.kln@gmail.com>
2021-09-09 05:31:15 +0000

    build out magic link auth strategy

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,11 +1,30 @@
 <script>
-    export let user 
+    import { Magic } from 'magic-sdk';
+    const m = new Magic('pk_live_D76A8E4360817CD3'); // ✨
+
+    export let user
+    $: userEmail = ''
+
+    const handleSignup = async() => {
+        // log in a user by their email
+        try {
+        const user = await m.auth.loginWithMagicLink({ email: userEmail });
+        console.log(user)
+        } catch(err) {
+            console.log(err)
+        // Handle errors if required!
+        }
+    }
 </script>
 
 <div class='main'>
     <div class='box'>
         <div class='container'>
             <button class='button primary' on:click={() => user.set(true)}>LOGIN</button>
+            <div class='row'>
+                <input type='email' placeholder='email' bind:value={userEmail}>
+                <button class='button primary' on:click={handleSignup}>sign up </button>
+            </div>
         </div>
     </div>
 </div>
